1e0c0d3637dad7bbc25bfa7fb6cff6b283973c77
Code-with-Nani <usmanhanan02@gmail.com>
2024-03-26 16:46:24 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+gsap.to(".hero2", {
+  x: 70,
+  y: -40,
+  duration: 3,
+  repeat: -1,
+  yoyo: true,
+  skew: 100,
+});
+gsap.to(".hero1", {
+  x: -40,
+  y: -40,
+  duration: 3,
+  repeat: -1,
+  yoyo: true,
+  skew: 100,
+});
